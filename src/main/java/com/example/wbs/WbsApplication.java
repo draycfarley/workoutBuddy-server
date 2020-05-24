aaaac69bd6895917af9400c374b89affd1946f59
cf446 <cf446@cornell.edu>
2020-05-24 10:22:19 -0400

    Added CORS acceptance for client

--- src/main/java/com/example/wbs/WbsApplication.java
+++ src/main/java/com/example/wbs/WbsApplication.java
@@ -2,12 +2,25 @@ package com.example.wbs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class WbsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WbsApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/workouts").allowedOrigins("https://workout-buddy-client2020.herokuapp.com/");
+			}
+		};
 	}
 
 }
