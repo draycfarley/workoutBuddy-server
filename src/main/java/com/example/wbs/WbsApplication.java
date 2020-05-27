48df28ee68ecefc661a471d954969e7d05a31e42
cf446 <cf446@cornell.edu>
2020-05-27 15:28:12 -0400

    Made CORS more liberal

--- src/main/java/com/example/wbs/WbsApplication.java
+++ src/main/java/com/example/wbs/WbsApplication.java
@@ -6,10 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.cors.*;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import java.util.*;
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.core.Ordered;
+
 
 @SpringBootApplication
 public class WbsApplication {
@@ -18,18 +15,18 @@ public class WbsApplication {
 		SpringApplication.run(WbsApplication.class, args);
 	}
 
-	@Bean
-    public FilterRegistrationBean corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("https://workout-buddy-client2020.herokuapp.com");
-        config.setAllowedMethods(Arrays.asList("POST", "OPTIONS", "GET", "DELETE", "PUT"));
-        config.setAllowedHeaders(Arrays.asList("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
-        source.registerCorsConfiguration("/**", config);
-        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        return bean;
-    }
+	@Configuration
+	public class MyConfiguration {
+
+		@Bean
+		public WebMvcConfigurer corsConfigurer() {
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**");
+				}
+			};
+		}
+	}
 
 }
